5440d5e17321e7f3729946b360b83576eaf0cec9
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-08-31 15:40:53 +0200

    Restrict deployable users

--- lib/model_five/commands/deploy.rb
+++ lib/model_five/commands/deploy.rb
@@ -12,6 +12,7 @@ module ModelFive
       }
 
       match(/deploy( (?<branch>\w*))?( to (?<env>\w*))?$/) do |client, data, match|
+        # Permit deploy only on restricted channels
         unless ModelFive.config.channel_ids.include? data.channel
           client.say :text => "<@#{data.user}>: not allowed to deploy from <##{data.channel}>",
                      :channel => data.channel
@@ -21,6 +22,16 @@ module ModelFive
 
         branch = match[:branch] || CONFIG[:branch]
         env = match[:env] || CONFIG[:env]
+
+        # Permit deploy to master only for restricted users
+        if env == 'prod'
+          unless ModelFive.config.admin_ids.include? data.user
+            client.say :text => "<@#{data.user}>: not allowed to deploy *#{branch}* to *#{env}*",
+                       :channel => data.channel
+
+            next
+          end
+        end
 
         client.say :text => "<@#{data.user}>: started deploy *#{branch}* to *#{env}*",
                    :channel => data.channel
