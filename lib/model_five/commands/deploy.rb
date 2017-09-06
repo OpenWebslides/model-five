36a1557cb938ed0075efa3150eeed7272d574aaa
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-09-06 13:31:27 +0200

    Rewrite deploy command

--- lib/model_five/commands/deploy.rb
+++ lib/model_five/commands/deploy.rb
@@ -3,12 +3,16 @@
 module ModelFive
   module Commands
     class Deploy < Command
-      match(/deploy( (?<branch>\w*))?( to (?<env>\w*))?$/) do |client, data, match|
-        # Default branch and environment
-        branch = match[:branch] || 'master'
-        env = match[:env] || 'dev'
-
+      command 'deploy' do |client, data, match|
         begin
+          expr = /^((?<branch>\w*)( to (?<env>\w*))?)?$/.match match[:expression]
+
+          # Default branch and environment
+          branch = (expr && expr[:branch]) || 'master'
+          env = (expr && expr[:env]) || 'dev'
+
+          raise ModelFive::Error, "Environment *#{env}* not found" unless ModelFive.config.environments[env]
+
           policy = Policies::Deploy.new :user => data.user,
                                         :branch => branch,
                                         :environment => env
